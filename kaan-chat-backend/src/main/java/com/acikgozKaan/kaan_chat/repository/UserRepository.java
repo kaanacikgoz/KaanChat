8bf15218df2427e7e28c1a80d74ac7d555b6b117
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-02-06 00:21:44 +0300

    Add user repo

---
+++ kaan-chat-backend/src/main/java/com/acikgozKaan/kaan_chat/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.acikgozKaan.kaan_chat.repository;
+
+import com.acikgozKaan.kaan_chat.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
