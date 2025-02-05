8f72e249b46d4a8905d650f6e125cbf393d017cb
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-02-06 00:29:41 +0300

    Add main class

---
+++ kaan-chat-backend/src/main/java/com/acikgozKaan/kaan_chat/KaanChatApplication.java
@@ -0,0 +1,13 @@
+package com.acikgozKaan.kaan_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaanChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaanChatApplication.class, args);
+	}
+
+}
