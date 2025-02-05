b00b72e0dbe9c123bc7b658999d93c61fc8794ae
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-02-06 01:22:22 +0300

    Add default test class

---
+++ kaan-chat-backend/src/test/java/com/acikgozKaan/kaan_chat/KaanChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acikgozKaan.kaan_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaanChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
