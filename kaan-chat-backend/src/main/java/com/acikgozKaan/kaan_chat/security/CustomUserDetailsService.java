ecead3afa683cdd6890aface684e321386c46282
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-02-06 00:38:38 +0300

    Add CustomUserDetailsService for user authentication

---
+++ kaan-chat-backend/src/main/java/com/acikgozKaan/kaan_chat/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.acikgozKaan.kaan_chat.security;
+
+import com.acikgozKaan.kaan_chat.entity.User;
+import com.acikgozKaan.kaan_chat.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+
+}
