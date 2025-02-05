c5e1c44aaa49ddfecfcec85990c14b15af0ba07f
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-02-06 00:43:18 +0300

    Add JwtUtil for JWT token generation, validation, and extraction

---
+++ kaan-chat-backend/src/main/java/com/acikgozKaan/kaan_chat/security/JwtUtil.java
@@ -0,0 +1,65 @@
+package com.acikgozKaan.kaan_chat.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+
+    @Value("${JWT_EXPIRATION_TIME}")
+    private long EXPIRATION_TIME;
+
+    @Value("${JWT_SECRET}")
+    private String SECRET;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SIGNATURE_ALGORITHM)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        try {
+            final String extractedUsername = extractUsername(token);
+            return (extractedUsername.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+}
