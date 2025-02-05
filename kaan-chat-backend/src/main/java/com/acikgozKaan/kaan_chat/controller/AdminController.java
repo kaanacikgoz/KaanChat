d1b7025333111e0989de8dc743a3a989204b41ba
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-02-06 01:12:02 +0300

    Add AdminController

---
+++ kaan-chat-backend/src/main/java/com/acikgozKaan/kaan_chat/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.acikgozKaan.kaan_chat.controller;
+
+import com.acikgozKaan.kaan_chat.entity.User;
+import com.acikgozKaan.kaan_chat.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+}
